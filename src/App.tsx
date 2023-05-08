907b8fff374281432d79ca525678ba5bd4cb20fc
Gunnar Rangoy <gunnar@rangoy.com>
2023-05-08 21:59:41 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SjakkMattMartin from './sjakkmattmartin'
+
+function App() {
+  return (
+    <>
+      <h1>SjakkMattMartin</h1>
+      <SjakkMattMartin></SjakkMattMartin>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/sjakkmattmartin.tsx
@@ -0,0 +1,9 @@
+import { Chessboard } from "react-chessboard";
+
+export default function SjakkMattMartin() {
+    return (
+      <div>
+        <Chessboard id="BasicBoard" />
+      </div>
+    );
+  }
