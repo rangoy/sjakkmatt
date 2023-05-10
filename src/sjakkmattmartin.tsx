62105c2a7ba5f6952bfc8aa441f643a1139d0bef
Gunnar Rangoy <gunnar.rangoy@knowit.no>
2023-05-10 06:20:57 +0200

    Spiller mot random

--- src/sjakkmattmartin.tsx
+++ src/sjakkmattmartin.tsx
@@ -1,9 +1,58 @@
+import { useState } from "react";
+import { Chess, Color, PartialMove } from "chess.ts"
 import { Chessboard } from "react-chessboard";
+import { Square } from "chess.js";
 
 export default function SjakkMattMartin() {
-    return (
-      <div>
-        <Chessboard id="BasicBoard" />
-      </div>
+  const [game, setGame] = useState(new Chess());
+
+  type player = 'computer' | 'human'
+  const gameConfig: Record<Color, player> = { b: 'computer', w: 'human' };
+
+  function makeAMove(move: PartialMove | string) {
+    setGame((game) => {
+      // const gameCopy = game.clone();
+      const result = game.move(move);
+
+      if (gameConfig[game.state.turn] === 'computer') {
+        setTimeout(makeRandomMove, 2000);
+      }
+
+      console.log(result, game)
+      return game.clone();
+      // if (result)
+      //   return gameCopy;
+      // else return game;
+    }
+
     );
+    // return result; // null if the move was illegal, the move object if the move was legal
   }
+
+  function makeRandomMove() {
+    console.log('makerandom2')
+
+    const possibleMoves = game.moves({ verbose: true }).filter(v => v.color === 'b');
+    console.log(possibleMoves)
+    if (game.gameOver() || game.inDraw() || possibleMoves.length === 0) return; // exit if the game is over
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    makeAMove(possibleMoves[randomIndex]);
+  }
+
+  function onDrop(sourceSquare: Square, targetSquare: Square) {
+    const move = makeAMove({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: "q", // always promote to a queen for example simplicity
+    });
+
+    // illegal move
+    // if (move === null) return false;
+    return true;
+  }
+
+  return <>
+    {game.inCheck()}
+    <Chessboard position={game.fen()} onPieceDrop={onDrop} />
+  </>;
+}
